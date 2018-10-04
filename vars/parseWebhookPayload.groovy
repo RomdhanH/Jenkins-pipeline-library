e7b92d05209a5bbc9fe184df0af4b7a898bb9428
Samir Saad <samir.saadallah@gmail.com>
2018-10-03 22:17:58 -0400

    revert body

--- vars/parseWebhookPayload.groovy
+++ vars/parseWebhookPayload.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/groovy
-def call(Map config) {
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
 	def payloadObject = new groovy.json.JsonSlurper().parseText(config.payload)
 	
