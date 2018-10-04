57661d3d20b74e1c84f4a6f45496e5ff00f93d7f
Samir Saad <samir.saadallah@gmail.com>
2018-10-03 21:48:47 -0400

    Init lib

---
+++ vars/parseWebhookPayload.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/groovy
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+	def payloadObject = new groovy.json.JsonSlurper().parseText(config.payload)
+	
+	env.repoURL = payloadObject.repository.clone_url                            
+	env.repoName = repoURL.tokenize('/').last().replace(".git", "")
+	env.appName = repoName
+	
+	def ref = payloadObject.ref
+	env.branch = ref.replace("refs/heads/", "")
+	
+	echo payloadString
+	echo repoName
+	echo branch
+}
