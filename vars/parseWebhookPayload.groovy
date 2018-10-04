0b40e04798f60330dba1938fc61817e26f0eced9
Samir Saad <samir.saadallah@gmail.com>
2018-10-03 22:28:44 -0400

    use map

--- vars/parseWebhookPayload.groovy
+++ vars/parseWebhookPayload.groovy
@@ -1,25 +1,21 @@
 #!/usr/bin/groovy
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-	echo body
-	echo config
-	echo config.payload
-	
-	def payloadObject = new groovy.json.JsonSlurper().parseText(config.payload)
-	
-	env.repoURL = payloadObject.repository.clone_url                            
-	env.repoName = repoURL.tokenize('/').last().replace(".git", "")
-	env.appName = repoName
-	
-	def ref = payloadObject.ref
-	env.branch = ref.replace("refs/heads/", "")
-	
-	echo payloadString
-	echo repoName
-	echo branch
+def call(Map config) {
+	node {
+		echo body
+		echo config
+		echo config.payload
+		
+		def payloadObject = new groovy.json.JsonSlurper().parseText(config.payload)
+		
+		env.repoURL = payloadObject.repository.clone_url                            
+		env.repoName = repoURL.tokenize('/').last().replace(".git", "")
+		env.appName = repoName
+		
+		def ref = payloadObject.ref
+		env.branch = ref.replace("refs/heads/", "")
+		
+		echo payloadString
+		echo repoName
+		echo branch
+	}
 }
