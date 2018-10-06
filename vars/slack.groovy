32427730b13b6c84d78c4f6fd72d92e7e4fd1b65
Samir Saad <samir.saadallah@gmail.com>
2018-10-05 22:45:05 -0400

    x

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,17 +1,27 @@
 #!/usr/bin/groovy
 
-def info(message) {
-    slackSend color: "46c9e2", message: message
+def info(message, appendBuildInfo) {
+    slackSend color: "46c9e2", message: appendInfo(message, appendBuildInfo)
 }
 
-def warning(message) {
-    slackSend color: "warning", message: message
+def warning(message, appendBuildInfo) {
+    slackSend color: "warning", message: appendInfo(message, appendBuildInfo)
 }
 
-def success(message) {
-    slackSend color: "good", message: message
+def success(message, appendBuildInfo) {
+    slackSend color: "good", message: appendInfo(message, appendBuildInfo)
 }
 
-def failure(message) {
-    slackSend color: "danger", message: message
+def failure(message, appendBuildInfo) {
+    slackSend color: "danger", message: appendInfo(message, appendBuildInfo)
+}
+
+def appendInfo(message, appendBuildInfo) {
+    if(appendBuildInfo){
+        return message +
+				"\n Job: ${JOB_NAME}" +
+				"\n App: ${repoName} (branch: ${branch})" +
+				"\n <${RUN_DISPLAY_URL}|Build #${BUILD_NUMBER}>  |  <${BUILD_URL}changes|Changes>  |  <${BUILD_URL}console|Logs>"
+    }
+    return message
 }
--- vars/slackBuildResult.groovy
+++ vars/slackBuildResult.groovy
@@ -5,15 +5,15 @@ def call(String buildResult) {
 	def message = appendBuildInfo "Job result: " + buildResult
 																
 	if ( buildResult == "SUCCESS") {
-		slack.success message
+		slack.success message, true
 	}
 	else if( buildResult == "FAILURE" || buildResult == "NOT_BUILT" ) { 
-		slack.failure message
+		slack.failure message, true
 	}
 	else if( buildResult == "UNSTABLE" || buildResult == "ABORTED" ) { 
-		slack.warning message
+		slack.warning message, true
 	}
 	else {
-		slack.failure message
+		slack.failure message, true
 	}
 }
