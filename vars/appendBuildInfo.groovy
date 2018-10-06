db30ee0e8e8dc000afe3d20ad4bf455806dcf6bd
Samir Saad <samir.saadallah@gmail.com>
2018-10-05 22:23:57 -0400

    x

--- vars/appendBuildInfo.groovy
+++ vars/appendBuildInfo.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/groovy
 
 def call(String message) {
-	def message = 	message +
+	def msg = 	message +
 					"\n Job: ${JOB_NAME}" +
 					"\n App: ${repoName} (branch: ${branch})" +
 					"\n <${RUN_DISPLAY_URL}|Build #${BUILD_NUMBER}>  |  <${BUILD_URL}changes|Changes>  |  <${BUILD_URL}console|Logs>"
+	return msg
 }
