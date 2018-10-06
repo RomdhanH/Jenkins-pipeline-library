d104053e20112a77796f9a44defcf6e64a00c201
Samir Saad <samir.saadallah@gmail.com>
2018-10-06 11:13:24 -0400

    add gitCheckout.groovy

--- vars/initCICD.groovy
+++ vars/initCICD.groovy
@@ -1,20 +1,9 @@
 #!/usr/bin/groovy
 
-def call(Map config) {
-	echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx"
-	env.cicdRepo = 'http://gogs-iamp.pathfinder.gov.bc.ca/iamp/cicd.git'
+def call() {
+	env.cicdRepoURL = 'http://gogs-iamp.pathfinder.gov.bc.ca/iamp/cicd.git'
 	
-	echo cicdRepo
-	
-	checkout([$class: 'GitSCM', 
-		branches: [[name: '*/master']], 
-		doGenerateSubmoduleConfigurations: false, 
-		extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'cicd']], 
-		submoduleCfg: [], 
-		userRemoteConfigs: [[credentialsId: 'jenkins-gogs', url: "${cicdRepo}"]]
-	])
+	gitCheckout repoURL: cicdRepoURL, branch: 'master', directory: 'cicd', credentialsId: 'jenkins-gogs'
 	
 	env.mvnCmd = "mvn -s $WORKSPACE/cicd/maven-settings.xml"
-	
-	echo mvnCmd
 }
