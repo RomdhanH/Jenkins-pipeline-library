4e03657845b8bf2cc35b10d831831a7d7f224e34
Samir Saad <samir.saadallah@gmail.com>
2018-10-10 11:26:11 -0400

    add more checks for files changes

--- vars/chechConfigChanges.groovy
+++ vars/chechConfigChanges.groovy
@@ -5,19 +5,23 @@ def call() {
 	
 	env.testProject = 'ag-pssg-is-test'
 	
-	env.cicdCommit = sh(script: 'cd cicd;git rev-parse --short HEAD;cd ..', returnStdout: true).trim()
-	echo cicdCommit
-	
+	dir('cicd') {
+		env.cicdCommit = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+		env.cicdChangedFile = sh(script: "git show --pretty="" --name-only ${cicdCommit}", returnStdout: true).trim()
+		echo cicdCommit
+		echo cicdChangedFile
+	}
+
 	openshift.withCluster() {
 		openshift.withProject(devProject) {			
-			env.bcDevChanged = !openshift.selector("bc", [app:"${appName}", commit:"${cicdCommit}"]).exists()
-			env.cmDevChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists()			
-			env.dcDevChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists()
+			env.bcDevChanged = (!openshift.selector("bc", [app:"${appName}", commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-image-builder-template.yaml')
+			env.cmDevChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-service-config-dev.yaml')
+			env.dcDevChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-spring-service-template.yaml')
 		}
 		
 		openshift.withProject(testProject) {
-			env.cmTestChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists()			
-			env.dcTestChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists()
+			env.cmTestChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-service-config-test.yaml')
+			env.dcTestChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-spring-service-template.yaml')
 		}
 	}
 	
