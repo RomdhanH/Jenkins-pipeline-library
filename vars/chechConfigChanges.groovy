e7a2624a39afbfa681e2f83ba1bd12e85d548ca1
Samir Saad <samir.saadallah@gmail.com>
2018-10-10 13:06:08 -0400

    fix checks

--- vars/chechConfigChanges.groovy
+++ vars/chechConfigChanges.groovy
@@ -8,20 +8,18 @@ def call() {
 	dir('cicd') {
 		env.cicdCommit = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 		env.cicdChangedFile = sh(script: "git show --pretty=\"\" --name-only ${cicdCommit}", returnStdout: true).trim()
-		echo cicdCommit
-		echo cicdChangedFile
 	}
 
 	openshift.withCluster() {
 		openshift.withProject(devProject) {			
-			env.bcDevChanged = !openshift.selector("bc", [app:"${appName}", commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-image-builder-template.yaml')
-			env.cmDevChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-service-config-dev.yaml')
-			env.dcDevChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-spring-service-template.yaml')
+			env.bcDevChanged = !openshift.selector("bc", [app:"${appName}", commit:"${cicdCommit}"]).exists()
+			env.cmDevChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists()
+			env.dcDevChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists()
 		}
 		
 		openshift.withProject(testProject) {
-			env.cmTestChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-service-config-test.yaml')
-			env.dcTestChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists() && cicdChangedFile.contains('iamp-spring-service-template.yaml')
+			env.cmTestChanged = !openshift.selector("cm", [commit:"${cicdCommit}"]).exists()
+			env.dcTestChanged = !openshift.selector("dc", [app:"${appName}", commit:"${cicdCommit}"]).exists()
 		}
 	}
 	
