9d13d65e933dc19b0df024b805197434947eda06
Samir Saad <samir.saadallah@gmail.com>
2018-10-10 14:58:23 -0400

    add soapui plugin version

--- vars/mavenSoapuiTests.groovy
+++ vars/mavenSoapuiTests.groovy
@@ -3,7 +3,7 @@
 def call(Map config) {
 	def soapuiProject = "src/test/soapui/" + appName + "-soapui-project.xml"
 	def soapuiProperties = "src/test/soapui/" + config.env + ".properties"
-	def command = "mvn com.smartbear.soapui:soapui-maven-plugin:" + config.goal + " -Dsoapui.project=" + soapuiProject + " -Dsoapui.properties=" + soapuiProperties
+	def command = "mvn com.smartbear.soapui:soapui-maven-plugin:5.4.0:" + config.goal + " -Dsoapui.project=" + soapuiProject + " -Dsoapui.properties=" + soapuiProperties
 	
 	sh command
 }
