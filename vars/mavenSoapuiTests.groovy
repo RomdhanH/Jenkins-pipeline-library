58fbc79737cb6b148afeb1efff45b8d16d623c60
Samir Saad <samir.saadallah@gmail.com>
2018-10-14 15:47:39 -0400

    add file checks

--- vars/mavenSoapuiTests.groovy
+++ vars/mavenSoapuiTests.groovy
@@ -3,7 +3,10 @@
 def call(Map config) {
 	def soapuiProject = "src/test/soapui/" + appName + "-soapui-project.xml"
 	def soapuiProperties = "src/test/soapui/" + config.env + ".properties"
-	def command = "mvn com.smartbear.soapui:soapui-maven-plugin:5.4.0:" + config.goal + " -Dsoapui.project=" + soapuiProject + " -Dsoapui.properties=" + soapuiProperties
 	
-	sh command
+	if (fileExists(soapuiProject) && fileExists(soapuiProperties)) {
+		def command = "mvn com.smartbear.soapui:soapui-maven-plugin:5.4.0:" + config.goal + " -Dsoapui.project=" + soapuiProject + " -Dsoapui.properties=" + soapuiProperties
+	
+		sh command
+	}
 }
