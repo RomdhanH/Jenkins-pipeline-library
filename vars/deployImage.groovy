b94aa15cde1f1dde32cd6ddea3d40f63334496b5
Samir Saad <samir.saadallah@gmail.com>
2018-10-15 07:57:50 -0400

    remove extra space

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -2,7 +2,7 @@
 
 def call(Map config) {
 				
-	def command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p IMAGE_NAME=${appName} -p IMAGE_TAG= " + config.version + " -p REPLICAS=" + config.replicas + " -l app=${appName},commit=${cicdCommit},hystrix.enabled=true | oc create -f- -n " + config.project + " || true"
+	def command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p IMAGE_NAME=${appName} -p IMAGE_TAG=" + config.version + " -p REPLICAS=" + config.replicas + " -l app=${appName},commit=${cicdCommit},hystrix.enabled=true | oc create -f- -n " + config.project + " || true"
 	
 	sh command
 	
