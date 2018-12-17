0d3e4f5c927f10fc5d327a86148fed04b8191e8c
hromdhane <houssem.romdhane@opticca.com>
2018-12-17 22:24:33 +0000

    Update 'vars/testpipeline.groovy'

--- vars/testpipeline.groovy
+++ vars/testpipeline.groovy
@@ -2,11 +2,10 @@
 //@Library('jenkins-pipeline-library')
 import hudson.model.*
 def call(Closure body) {
-    stage ('Build')
+    
         node ('maven') {
             checkout scm
             initCICD()
             sh 'mvn clean package'
       }
 //  body()
-}
