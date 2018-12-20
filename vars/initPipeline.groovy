52978fdc32c4f11aa1337e5d9855c11ba926e3ff
hromdhane <houssem.romdhane@opticca.com>
2018-12-20 18:54:53 +0000

    Update 'vars/initPipeline.groovy'

--- vars/initPipeline.groovy
+++ vars/initPipeline.groovy
@@ -2,7 +2,14 @@
 
 def call() {
 	def payloadString = "$payload"
-	parseWebhookPayload payload: payloadString
+	//parseWebhookPayload payload: payloadString
+  
+    env.repoURL = REPO_URL
+    print REPO_URL
+	env.repoName = repoURL.tokenize('/').last().replace(".git", "")
+	env.appName = repoName
+	
+	env.branch = BRANCH_NAME
 	
 	currentBuild.displayName = repoName + ":" + branch + " #" + BUILD_NUMBER
 	currentBuild.description = "Building repository: " + repoName + ", branch: " + branch
