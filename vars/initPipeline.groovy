93106577836b26f2bcf85dc1668e26e9809eb091
hromdhane <houssem.romdhane@opticca.com>
2019-01-08 20:59:50 +0000

    Update 'vars/initPipeline.groovy'

--- vars/initPipeline.groovy
+++ vars/initPipeline.groovy
@@ -1,11 +1,17 @@
 #!/usr/bin/groovy
 
 def call() {
-	def payloadString = "$payload"
-	parseWebhookPayload payload: payloadString
+	//def payloadString = "$payload"
+	//parseWebhookPayload payload: payloadString
+  
+    //env.repoURL = payloadObject.repository.clone_url                            
+	env.repoName = readMavenPom().getArtifactId() //repoURL.tokenize('/').last().replace(".git", "")
+	env.appName = repoName
 	
-	currentBuild.displayName = repoName + ":" + branch + " #" + BUILD_NUMBER
-	currentBuild.description = "Building repository: " + repoName + ", branch: " + branch
+	env.branch = BRANCH_NAME //ref.replace("refs/heads/", "")
+	
+	//currentBuild.displayName = repoName + ":" + branch + " #" + BUILD_NUMBER
+	//currentBuild.description = "Building repository: " + repoName + ", branch: " + branch
 	
 	slack.info message: 'STARTED', appendBuildInfo: true
 }
