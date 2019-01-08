1344f52e4ce2905907f3825ae1d77696b0af13e0
hromdhane <houssem.romdhane@opticca.com>
2019-01-08 21:03:49 +0000

    Update 'vars/testpipeline2.groovy'

--- vars/testpipeline2.groovy
+++ vars/testpipeline2.groovy
@@ -4,14 +4,12 @@ pipeline {
   
 	agent { node { label 'maven' } }
 
-  
- 
 	stages {
       
 		stage('Init Pipeline') {
             steps {
              parallel (
-                    "Parse WebHook": {
+                    "Init Pipeline": {
                         initPipeline()
                     },
                     "Init CICD": {
@@ -24,91 +22,13 @@ pipeline {
         
         stage ("Checkout & Build") {
             steps {
-              gitCheckout repoURL: repoURL, branch: branch, directory: appName, credentialsId: 'jenkins-gogs'
+              //gitCheckout repoURL: repoURL, branch: branch, directory: appName, credentialsId: 'jenkins-gogs'
               
                 dir("${appName}") {
                     mavenBuild()
                 }
             }
         }
-		
-		stage('Check Config Changes') {
-            when {
-                expression { return branch == "develop" }
-            }
-            steps {
-                chechConfigChanges()
-            }
-        }
-			  
-        stage("Build Image") {
-            when {
-                expression { return branch == "develop" }
-            }
-            steps {
-                buildDockerImage()
-            }
-        }
-		
-		stage('Cleanup Dev') {
-			when {
-				expression{ return (branch == "develop" && devChanged.toBoolean()) }
-			}
-			steps {
-				cleanConfig(devProject)
-			}
-        }
-		
-        stage("Deploy to Dev") {
-            when {
-                expression { return branch == "develop" }
-            }
-            steps {
-				sh "oc process -f cicd/iamp-service-config-dev.yaml -l commit=${cicdCommit} | oc create -f- -n ${devProject} || true"
-				
-				deployImage project: devProject, version: 'latest', replicas: 1
-            }
-        }
-		
-		stage ('Promote to Test') {			
-			when {
-                expression { return branch == "develop" }
-            }
-			steps {
-				timeout(time:30, unit:'MINUTES') {
-					input message: "Promote to Test?", ok: "Promote"
-				}
-				script{
-					// Tag for Test
-					openshift.withCluster() {
-						openshift.tag("${devProject}/${appName}:latest", "${testProject}/${appName}:${version}")
-                    }
-				}
-			}
-		}
-		
-		/*stage('Cleanup Test') {
-			when {
-				expression{ return (branch == "develop" && testChanged.toBoolean()) }
-			}
-			steps {
-				cleanConfig(testProject)
-			}
-        }
-		
-		stage("Deploy to Test") {
-            when {
-                expression { return branch == "develop" }
-            }
-            steps {
-			
-				sh "oc delete hpa -l app=${appName} -n ${testProject} || true"
-				
-				sh "oc process -f cicd/iamp-service-config-test.yaml -l commit=${cicdCommit} | oc create -f- -n ${testProject} || true"
-				
-				deployImage project: testProject, version: version, replicas: 2
-            }
-        }*/
     }
 	
 	post('Publish Results') {
