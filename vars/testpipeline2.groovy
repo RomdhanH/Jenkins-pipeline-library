1886f4748a0b6cf59a72112186019247b6f4a693
hromdhane <houssem.romdhane@opticca.com>
2019-01-08 16:59:08 +0000

    Update 'vars/testpipeline2.groovy'

--- vars/testpipeline2.groovy
+++ vars/testpipeline2.groovy
@@ -12,9 +12,9 @@ pipeline {
 		stage('Init Pipeline') {
             steps {
                
-                    "Init CICD": {
+                    
                         initCICD()
-                    }
+                    
                 
             }
         }
