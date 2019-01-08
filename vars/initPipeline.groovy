e361f59ee3b6ff7df95aed42585b5ff69c54cab1
hromdhane <houssem.romdhane@opticca.com>
2019-01-08 21:12:50 +0000

    Update 'vars/initPipeline.groovy'

--- vars/initPipeline.groovy
+++ vars/initPipeline.groovy
@@ -5,7 +5,9 @@ def call() {
   sh "ls -la"
   
 	env.repoName = readMavenPom().getArtifactId()
-  //sh "mv * ./$repoName"
+  sh "mv * ./$repoName"
+  sh "ls -la"
+  sh "ls -la $repoName"
 	env.appName = repoName
 	
 	env.branch = BRANCH_NAME 
