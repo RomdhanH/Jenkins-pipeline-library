779fd4bf03c02c7bd67755f11b14526ddb1a7c73
hromdhane <houssem.romdhane@opticca.com>
2019-01-09 04:08:59 +0000

    Update 'vars/testpipeline2.groovy'

--- vars/testpipeline2.groovy
+++ vars/testpipeline2.groovy
@@ -33,16 +33,15 @@ pipeline {
       		sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
    	     		}
           }
-        }
+        
       
 	
 		post('Publish Results') {
-          steps {
         always {
             slackBuildResult()
         	  }
-          }
     	}
     }
+    
 }
 }
