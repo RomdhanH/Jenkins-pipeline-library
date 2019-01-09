5398e8b3e5df8644d37f1c165f0a1077f4664f9d
hromdhane <houssem.romdhane@opticca.com>
2019-01-09 04:10:03 +0000

    Update 'vars/testpipeline2.groovy'

--- vars/testpipeline2.groovy
+++ vars/testpipeline2.groovy
@@ -27,13 +27,7 @@ pipeline {
                 }
             }
         }
-        stage("SonarQube analysis") {
-          steps {
-    		withSonarQubeEnv('Sonar') {
-      		sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
-   	     		}
-          }
-        
+      
       
 	
 		post('Publish Results') {
