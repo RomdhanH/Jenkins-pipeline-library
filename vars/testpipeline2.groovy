a459a1cd631229afad6ee9ccf3bc03f388dce019
hromdhane <houssem.romdhane@opticca.com>
2019-01-09 04:57:16 +0000

    Update 'vars/testpipeline2.groovy'

--- vars/testpipeline2.groovy
+++ vars/testpipeline2.groovy
@@ -28,17 +28,21 @@ pipeline {
             }
         }
        stage('SonarQube analysis') {
+         steps {
    			 withSonarQubeEnv('Sonar') {
     		  sh 'mvn clean package sonar:sonar'
    			 } // SonarQube taskId is automatically attached to the pipeline context
   			}
+       }
       stage("Quality Gate"){
+        steps {
  			 timeout(time: 1, unit: 'HOURS') {
    			 def qg = waitForQualityGate() 
    			 if (qg.status != 'OK') {
      		 error "Pipeline aborted due to quality gate failure: ${qg.status}"
    		 		}
   			}
+        }
 		}
       
       
