5b5cceae3b9c0cd1c077fc956224c8e77e570ab5
hromdhane <houssem.romdhane@opticca.com>
2019-01-14 23:29:14 +0000

    Update 'vars/buildDockerImage.groovy'

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -8,8 +8,8 @@ def call(project) {
 	sh "cp ${appJar} oc-build/deployments/"
 	
 	// Create build and image streams
-	sh "oc new-build --name=${appName} registry.access.redhat.com/jboss-fuse-6/fis-java-openshift:2.0 --binary=true --labels=app=${appName} -n project || true"
+	sh "oc new-build --name=${appName} registry.access.redhat.com/jboss-fuse-6/fis-java-openshift:2.0 --binary=true --labels=app=${appName} -n ${project} || true"
 	
 	// Start the build
-	sh "oc start-build ${appName} --from-dir=oc-build/deployments --wait=true -n project"
+  sh "oc start-build ${appName} --from-dir=oc-build/deployments --wait=true -n ${project} "
 }
