4dfb91dde7b158605155dc1ba1bb3b62651113c0
hromdhane <houssem.romdhane@opticca.com>
2019-01-14 21:13:59 +0000

    Update 'vars/IampServicePipeline.groovy'

--- vars/IampServicePipeline.groovy
+++ vars/IampServicePipeline.groovy
@@ -118,6 +118,7 @@ pipeline {
                 expression { return branch == "develop" }
             }
             steps {
+              def version    = getVersionFromPom("./pom.xml")
 				sh "oc process -f cicd/iamp-service-config-dev.yaml -l commit=${cicdCommit} | oc create -f- -n ${testProject} || true"
 				
               deployImage project: testProject, version: ${version}, replicas: 1
