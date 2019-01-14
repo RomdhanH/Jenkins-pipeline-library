fe307a598be9f77d77aef8f1e1c32b6540a76c94
hromdhane <houssem.romdhane@opticca.com>
2019-01-14 14:16:49 +0000

    Update 'vars/IampServicePipeline.groovy'

--- vars/IampServicePipeline.groovy
+++ vars/IampServicePipeline.groovy
@@ -70,19 +70,25 @@ pipeline {
         }
       stage('Determine Deployment color') {
     // Determine current project
+         when {
+                expression { return branch == "develop" }
+            }
+        steps {
     sh "oc get project|grep -v NAME|awk '{print \$1}' >project.txt"
     project = readFile('project.txt').trim()
     sh "oc get route example -n ${project} -o jsonpath='{ .spec.to.name }' > activesvc.txt"
-
+        
     // Determine currently active Service
     active = readFile('activesvc.txt').trim()
     if (active == "example-green") {
       dest = "example-blue"
+    
     }
     echo "Active svc: " + active
     echo "Dest svc:   " + dest
     echo "New color:  " + newcolor
-  }
+        }
+        }
       stage("Build Image") {
             when {
                 expression { return branch == "develop" }
