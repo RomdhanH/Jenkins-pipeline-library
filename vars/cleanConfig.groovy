5407aa0902dacae19e2ad0e7cf73a868a1b134b0
hromdhane <houssem.romdhane@opticca.com>
2019-01-14 16:41:04 +0000

    Update 'vars/cleanConfig.groovy'

--- vars/cleanConfig.groovy
+++ vars/cleanConfig.groovy
@@ -5,14 +5,14 @@ def call(project) {
 	def command
 	
 	// Clean ConfigMap
-	if((project == devProject && cmDevChanged.toBoolean()) /*|| (project == testProject && cmTestChanged.toBoolean())*/){
+	if((project == devProject && cmDevChanged.toBoolean()) || (project == testProject && cmTestChanged.toBoolean())){
 		command = "oc delete cm iamp-service-config -n " + project + " || true"
 		
 		sh command
 	}
 	
 	// Clean deployment configurations
-	if((project == devProject && dcDevChanged.toBoolean()) /*|| (project == testProject && dcTestChanged.toBoolean())*/){
+	if((project == devProject && dcDevChanged.toBoolean()) || (project == testProject && dcTestChanged.toBoolean())){
 		command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p IMAGE_NAME=${appName} -p IMAGE_TAG=latest | oc delete -f- -n " + project + " || true"
 		
 		sh command
