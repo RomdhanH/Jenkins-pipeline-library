b98df7fd7bfc2888ace1acd8f5a20a641ed2ced1
hromdhane <houssem.romdhane@opticca.com>
2019-01-15 21:13:51 +0000

    Update 'vars/switchroute.groovy'

--- vars/switchroute.groovy
+++ vars/switchroute.groovy
@@ -1,7 +1,7 @@
 def call(project) {
 def dest = "${appName}-${apiVersion}-${buildVersion_lowercase}"
 input "Switch Production?"
-    sh 'oc patch route ${appName}-${apiVersion} -p \'{"spec":{"to":{"name":"' + dest + '"}}}\'' -n ${testProject}
+    sh 'oc patch route ${appName}-${apiVersion} -p \'{"spec":{"to":{"name":"' + dest + '"}}}\' -n ${testProject}'
     sh 'oc get route ${appName}-${apiVersion} -o yaml'
     
   
