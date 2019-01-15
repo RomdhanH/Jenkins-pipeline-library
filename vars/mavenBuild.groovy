24d8607968ec531dcaa8c24694b8cf8dd840580c
hromdhane <houssem.romdhane@opticca.com>
2019-01-15 14:44:41 +0000

    Update 'vars/mavenBuild.groovy'

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -5,6 +5,7 @@ def call() {
 	env.groupId = readMavenPom().getGroupId()
 	env.artifactId = readMavenPom().getArtifactId()
 	env.version = readMavenPom().getVersion()
+  	env.buildVersion_lowercase = version.toLowerCase()
 	env.name = readMavenPom().getName()
 						
 	sh "mvn clean package -DskipTests=true"
