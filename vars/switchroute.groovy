18b266ae16fb51cf062bc6e46b718bbe2dd89cad
hromdhane <houssem.romdhane@opticca.com>
2019-01-15 16:57:34 +0000

    Update 'vars/switchroute.groovy'

--- vars/switchroute.groovy
+++ vars/switchroute.groovy
@@ -2,8 +2,7 @@ def call(project) {
 def dest = "${appName}-${apiVersion}-${buildVersion_lowercase}"
 input "Switch Production?"
     sh 'oc patch route ${appName}-${apiVersion} -p \'{"spec":{"to":{"name":"' + dest + '"}}}\''
-    sh 'oc get route ${appName}-${apiVersion} > oc_out.txt'
-    oc_out = readFile('oc_out.txt')
-    echo "Current route configuration: " + oc_out
+    sh 'oc get route ${appName}-${apiVersion} -o yaml'
+    
   
 }
