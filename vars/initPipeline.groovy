1ca78337336fb7772fa4e5eabeed534d0576202f
hromdhane <houssem.romdhane@opticca.com>
2019-01-15 04:17:01 +0000

    Update 'vars/initPipeline.groovy'

--- vars/initPipeline.groovy
+++ vars/initPipeline.groovy
@@ -1,17 +1,7 @@
 #!/usr/bin/groovy
 
 def call() {  
-  def content = new ConfigSlurper().parse(new File("https://github.com/HoussemRomdhane/Demo_Spring_Jenkins/blob/develop/src/main/resources/application.properties").toURL())
-   //def content = readFile 'https://github.com/HoussemRomdhane/Demo_Spring_Jenkins/blob/develop/src/main/resources/application.properties'
-
-Properties properties = new Properties()
-InputStream is = new ByteArrayInputStream(content.getBytes());
-properties.load(is)
-
-def runtimeString = 'SERVICE_VERSION'
-echo properties."$service.version"
-SERVICE_VERSION = properties."$service.version"
-echo SERVICE_VERSION
+  
   
   sh "ls -la"
   
@@ -26,6 +16,17 @@ echo SERVICE_VERSION
 	env.appName = repoName
 	
 	env.branch = BRANCH_NAME 
+  
+   def content = readFile '$repoName/src/main/resources/application.properties'
+
+Properties properties = new Properties()
+InputStream is = new ByteArrayInputStream(content.getBytes());
+properties.load(is)
+
+def runtimeString = 'SERVICE_VERSION'
+echo properties."$service.version"
+SERVICE_VERSION = properties."$service.version"
+echo SERVICE_VERSION
 	
 	slack.info message: 'STARTED', appendBuildInfo: true
 }
