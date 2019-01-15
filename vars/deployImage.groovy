10dfed0375478dda1222632e974cef92d35ff0ef
hromdhane <houssem.romdhane@opticca.com>
2019-01-15 14:31:31 +0000

    Update 'vars/deployImage.groovy'

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -3,16 +3,16 @@
 def call(Map config) {
   
 				
-  def command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p IMAGE_NAME=${appName} -p IMAGE_TAG=" + config.version + " -p REPLICAS=" + config.replicas + " -l app=${appName},commit=${cicdCommit},hystrix.enabled=true | oc create -f- -n " + config.project + " || true"
+  def command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p API_VERSION=${apiVersion} -p BUILD_VERSION=${version} -p IMAGE_NAME=${appName} -p IMAGE_TAG=" + config.version + " -p REPLICAS=" + config.replicas + " -l app=${appName},commit=${cicdCommit},hystrix.enabled=true | oc create -f- -n " + config.project + " || true"
 	
 	sh command
 	
 	script{
 		openshift.withCluster() {
 			openshift.withProject(config.project) {
-				def dcSelector = openshift.selector("dc", "${appName}")
+				def dcSelector = openshift.selector("dc", "${appName}-${apiVersion}-${version}")
 				dcSelector.rollout().latest()
-				def deployment = "${appName}-${dcSelector.object().status.latestVersion}"
+				def deployment = "${appName}-${apiVersion}-${version}-${dcSelector.object().status.latestVersion}"
 				def pods = openshift.selector('pods', [deployment: "${deployment}"])
 				
 				timeout(5) {
