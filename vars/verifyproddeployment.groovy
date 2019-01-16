cdc3d88eaff0c4426236401dbfb08e3d767e3244
hromdhane <houssem.romdhane@opticca.com>
2019-01-16 05:58:44 +0000

    Update 'vars/verifyproddeployment.groovy'

--- vars/verifyproddeployment.groovy
+++ vars/verifyproddeployment.groovy
@@ -3,9 +3,10 @@ def call () {
 
 try {
             input id: 'Rollback', message: 'Is the realise fine? Proceed with the rollback?', ok: 'Rollback!'
-            sh "ls -la"
+            sh  'oc patch route ${appName}-${apiVersion} -p \'{"spec":{"to":{"name":"' + dest + '"}}}\' -n ${testProject} || true'
+  			sh 'oc patch route ${appName}-${apiVersion}-${buildVersion_lowercase} -p \'{"spec":{"to":{"name":"' + xy + '"}}}\' -n ${testProject} || true'
         } catch (error) {
-           sh "ls -la"
+         sh  'oc delete pods,services -l name=${appName}-${apiVersion}-${buildVersion_lowercase}'
         }
   
 }
