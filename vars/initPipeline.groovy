752263a849cbe88fff52fbf3c1189be09c1d75ec
hromdhane <houssem.romdhane@opticca.com>
2019-01-16 03:50:31 +0000

    Update 'vars/initPipeline.groovy'

--- vars/initPipeline.groovy
+++ vars/initPipeline.groovy
@@ -3,34 +3,34 @@
 def call() {  
   
   
-  sh "ls -la"
-  
-  	env.version = readMavenPom().getVersion()
-	env.repoName = readMavenPom().getArtifactId()
-  	//env.APIVersion = 
-  //sh "printenv | sort"
-  sh "mkdir $repoName"
-  sh "rsync -a * ./$repoName"
-  sh "ls -la"
-  sh "pwd"
-  sh "ls -la $repoName"
-	env.appName = repoName
-	
-	env.branch = BRANCH_NAME 
-  env.apiVersion = "v1"
-  File propertiesFile = new File('hello-world/src/main/resources/application.properties')
-def config = new ConfigSlurper().parse(propertiesFile.toURL())
-println(config.service.version)
-  //def content = readFile 'hello-world/src/main/resources/application.properties'
-/*
-Properties properties = new Properties()
-InputStream is = new ByteArrayInputStream(content.getBytes());
-properties.load(is)
+        //sh "ls -la"
 
-def runtimeString = 'SERVICE_VERSION'
-echo properties."$service.version"
-SERVICE_VERSION = properties."$service.version"
-echo SERVICE_VERSION*/
-	
-	slack.info message: 'STARTED', appendBuildInfo: true
+        env.version = readMavenPom().getVersion()
+        env.repoName = readMavenPom().getArtifactId()
+        //env.APIVersion = 
+        //sh "printenv | sort"
+        sh "mkdir $repoName"
+        sh "rsync -a * ./$repoName"
+
+env.WORKSPACE = pwd()
+def version = readFile "${env.WORKSPACE}/src/main/resources/application.properties"
+
+
+
+        env.appName = repoName
+        env.branch = BRANCH_NAME 
+        env.apiVersion = "v1"
+
+      	//def content = readFile 'hello-world/src/main/resources/application.properties'
+        /*
+        Properties properties = new Properties()
+        InputStream is = new ByteArrayInputStream(content.getBytes());
+        properties.load(is)
+
+        def runtimeString = 'SERVICE_VERSION'
+        echo properties."$service.version"
+        SERVICE_VERSION = properties."$service.version"
+        echo SERVICE_VERSION*/
+
+        slack.info message: 'STARTED', appendBuildInfo: true
 }
