7a5b10c088043fb8f0b27743313c7c3cbe25c415
hromdhane <houssem.romdhane@opticca.com>
2019-01-16 18:43:16 +0000

    add tag to deployment

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -3,7 +3,7 @@
 def call(Map config) {
   
 				
-  def command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p API_VERSION=${apiVersion} -p BUILD_VERSION=${buildVersion_lowercase} -p IMAGE_NAME=${appName}-${apiVersion} -p IMAGE_TAG=" + config.version + " -p REPLICAS=" + config.replicas + " -l app=${appName}-${apiVersion},commit=${cicdCommit},hystrix.enabled=true | oc create -f- -n " + config.project + " || true"
+  def command = "oc process -f cicd/iamp-spring-service-template.yaml -p APP_NAME=${appName} -p API_VERSION=${apiVersion} -p BUILD_VERSION=${buildVersion_lowercase} -p IMAGE_NAME=${appName}-${apiVersion} -p IMAGE_TAG=" + config.version + " -p REPLICAS=" + config.replicas + " -l app=${appName}-${apiVersion},release=${buildVersion_lowercase},commit=${cicdCommit},hystrix.enabled=true | oc create -f- -n " + config.project + " || true"
 	
 	sh command
 	
