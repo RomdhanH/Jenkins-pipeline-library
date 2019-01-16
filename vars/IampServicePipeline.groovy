f4ce0c255d5c86d40919b86bd24b00d5e36521e5
hromdhane <houssem.romdhane@opticca.com>
2019-01-16 05:45:26 +0000

    Update 'vars/IampServicePipeline.groovy'

--- vars/IampServicePipeline.groovy
+++ vars/IampServicePipeline.groovy
@@ -20,14 +20,14 @@ pipeline {
             	}
          }
       stage ('Verify'){
-        steps {
+        
         try {
             input id: 'Deploy', message: 'Is Blue node fine? Proceed with Green node deployment?', ok: 'Deploy!'
               sh "ls -la"
         } catch (error) {
             sh "ls -la"
         }
-        }
+        
     }
  
         
