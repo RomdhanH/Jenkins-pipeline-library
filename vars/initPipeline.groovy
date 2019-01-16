4deb491f7123a9d05735a683e585d49a7125d464
hromdhane <houssem.romdhane@opticca.com>
2019-01-16 03:36:02 +0000

    Update 'vars/initPipeline.groovy'

--- vars/initPipeline.groovy
+++ vars/initPipeline.groovy
@@ -20,7 +20,7 @@ def call() {
   env.apiVersion = "v1"
   
   def content = readFile 'hello-world/src/main/resources/application.properties'
-
+/*
 Properties properties = new Properties()
 InputStream is = new ByteArrayInputStream(content.getBytes());
 properties.load(is)
@@ -28,7 +28,7 @@ properties.load(is)
 def runtimeString = 'SERVICE_VERSION'
 echo properties."$service.version"
 SERVICE_VERSION = properties."$service.version"
-echo SERVICE_VERSION
+echo SERVICE_VERSION*/
 	
 	slack.info message: 'STARTED', appendBuildInfo: true
 }
