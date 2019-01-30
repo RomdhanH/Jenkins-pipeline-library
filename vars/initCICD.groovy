1940f7ab55ce9cb8a3887c0f346b8fb4320d7784
RomdhanH <42674755+RomdhanH@users.noreply.github.com>
2019-01-30 10:54:40 -0500

    add credentials Id

--- vars/initCICD.groovy
+++ vars/initCICD.groovy
@@ -3,7 +3,7 @@
 def call() {
 	env.cicdRepoURL = 'https://github.com/RomdhanH/cicd.git'
 	
-	//gitCheckout repoURL: cicdRepoURL, branch: 'master', directory: 'cicd', credentialsId: 'iamp-jenkins'
+	gitCheckout repoURL: cicdRepoURL, branch: 'master', directory: 'cicd', credentialsId: 'iamp-jenkins'
 	
 	sh "cp -f $WORKSPACE/cicd/maven-settings.xml /home/jenkins/.m2/settings.xml"
 }
